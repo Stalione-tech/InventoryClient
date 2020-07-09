675b3ef44de0589028953fd8a6a8a24577078b81
abc <abc@example.com>
2020-07-09 12:02:30 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: 'products', component: ProductsComponent },
+    { path: '', component: ProductsComponent },
+    // { path: '**', component: ProductsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
